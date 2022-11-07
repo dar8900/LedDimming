8d8f89776ea9b3facdffd708f60da3949e0766d2
Dario Sadel VB <dar8900@gmail.com>
2022-11-07 12:15:31 +0100

    Eliminato riferimento all'utilizzo della libraria di debug, fix nome include

--- led_dimming.cpp
+++ led_dimming.cpp
@@ -1,5 +1,4 @@
-#include "led_stripes.h"
-#include "../SerialDebug/serial_debug.h"
+#include "led_dimming.h"
 
 uint16_t LedDimming::_percToAnalogWrite(uint8_t Perc)
 {
@@ -30,11 +29,7 @@ void LedDimming::setDimmingTime(uint16_t Time)
 		Time == NO_DIMMING)
 	{
 		_dimmingTime = Time;
-		if(_dimmingTime == NO_DIMMING)
-		{
-			Debug.logInfo("Dimming impostato a \"NO_DIMMING\"");
-		}
-		else
+		if(_dimmingTime != NO_DIMMING)
 		{
 			_brightnessIncrement = _pwmRange / (_dimmingTime / _DIMMING_CYCLE);
 		}
@@ -64,11 +59,6 @@ void LedDimming::setStatus(stripe_status NewStatus, bool Fast)
 			_stripeIsSwitching = false;
 		}
 		_targetStatus = NewStatus;
-		if(_ledStripeName)
-		{
-			Debug.logDebug("Impostazione nuovo status della striscia: " + String(_ledStripeName));
-		}
-		Debug.logDebug("Settato nuovo stato della striscia led al valore: " + String(NewStatus));
 	}
 }
 
@@ -88,11 +78,6 @@ void LedDimming::setBrightness(uint8_t NewBrightnessPerc)
 	if(AnalogBright != _brightnessTarget && NewBrightnessPerc <= MAX_BRIGHTNESS)
 	{
 		_brightnessTarget = AnalogBright;
-		if(_ledStripeName)
-		{
-			Debug.logDebug("Impostazione brightness striscia: " + String(_ledStripeName));
-		}
-		Debug.logInfo("Impostata nuova luminosita della striscia a: " + String(NewBrightnessPerc));
 	}
 }
 
@@ -113,13 +98,11 @@ void LedDimming::ledStripeEngine()
 				{
 					analogWrite(_pin, 0);
 					_actualBrightness = 0;
-					Debug.logInfo("Striscia spenta senza dimming");
 				}
 				else
 				{
 					analogWrite(_pin, _brightnessTarget);
 					_actualBrightness = _brightnessTarget;
-					Debug.logInfo("Striscia accesa senza dimming");
 				}
 				_actualStatus = _targetStatus;
 			}
@@ -137,7 +120,6 @@ void LedDimming::ledStripeEngine()
 						_actualStatus = _targetStatus;
 						_actualBrightness = 0;
 						_stripeIsSwitching = false;
-						Debug.logInfo("Striscia spenta con dimming");
 					}
 				}
 				else
@@ -152,7 +134,6 @@ void LedDimming::ledStripeEngine()
 						_actualStatus = _targetStatus;
 						_actualBrightness = _brightnessTarget;
 						_stripeIsSwitching = false;
-						Debug.logInfo("Striscia accesa con dimming");
 					}
 				}
 				analogWrite(_pin, _actualBrightness);
